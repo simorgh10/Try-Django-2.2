9d81fcc793d07dff9adb186389bb7070a99b33f2
simorgh10 <None>
2020-09-10 19:12:02 +0200

    Forced lookup field to be unique.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,16 +1,12 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
 
 
 def blog_post_detail_page(request, slug):
-    queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() >= 1:
-        obj = queryset.first()
-    else:
-        raise Http404
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = "blog_post_detail_page.html"
     context = {"obj": obj}
     return render(request, template_name, context)
