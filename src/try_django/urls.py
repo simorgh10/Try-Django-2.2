6f8c2d20f80e58a3af1050e64c28d7a3ec1ce34d
simorgh10 <None>
2020-09-10 14:34:47 +0200

    Use example for re_path

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
     path('', home_page),
+    re_path(r'^pages?/', home_page),
     path('about/', about_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
