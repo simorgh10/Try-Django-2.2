836f71571810e1e3f7ca9c015d03fcc13a1f6087
simorgh10 <None>
2020-09-10 18:44:00 +0200

    Handle non existing item error 2.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,15 +1,12 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
 
 
 def blog_post_detail_page(request, post_id):
-    try:
-        obj = BlogPost.objects.get(id=post_id)
-    except BlogPost.DoesNotExist:
-        raise Http404
+    obj = get_object_or_404(BlogPost, id=post_id)
     template_name = "blog_post_detail_page.html"
     context = {"obj": obj}
     return render(request, template_name, context)
