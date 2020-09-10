99340273a90f4faac8fc2b14b1fdd2640ff07432
simorgh10 <None>
2020-09-10 20:40:03 +0200

    Added basic form in contact view.

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -10,11 +10,12 @@ def about_page(request):
     return render(request, "about.html", {"title": "About Us"})
 
 def contact_page(request):
-    return render(request, "hello_world.html", {"title": "Contact Us"})
+    print(request.path, request.method, request.user, request.POST)
+    return render(request, "contact.html", {"title": "Contact Us"})
 
 def example_page(request):
     context = {"title": "example"}
-    template_name = "hello_world.html"
+    template_name = "contact.html"
     template_obj = get_template(template_name)
     rendered_item = template_obj.render(context)
     return HttpResponse(rendered_item)
