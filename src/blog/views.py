4e03798fffa47111802b193a14b399cf822b251e
simorgh10 <None>
2020-09-10 20:20:52 +0200

    Moved blog app templates to blog app.

--- src/blog/views.py
+++ src/blog/views.py
@@ -6,30 +6,30 @@ from .models import BlogPost
 
 def blog_post_list_view(request):
     qs = BlogPost.objects.all()
-    template_name = "blog_post_list.html"
+    template_name = "blog/list.html"
     context = {"object_list": qs}
     return render(request, template_name, context)
 
 def blog_post_create_view(request):
-    template_name = "blog_post_create.html"
+    template_name = "blog/create.html"
     context = {"form": None}
     return render(request, template_name, context)
 
 def blog_post_detail_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_post_detail.html"
+    template_name = "blog/detail.html"
     context = {"obj": obj}
     return render(request, template_name, context)
 
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_post_update.html"
+    template_name = "blog/update.html"
     context = {"obj": obj, "form": None}
     return render(request, template_name, context)
 
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_post_delete.html"
+    template_name = "blog/delete.html"
     context = {"obj": obj}
     return render(request, template_name, context)
 
