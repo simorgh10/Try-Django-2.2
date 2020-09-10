eb56fb3df6537f34b202423d357ddef7e67bfa05
simorgh10 <None>
2020-09-10 21:15:58 +0200

    Changed contact basic form to Django form.

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
 
+from .forms import ContactForm
+
 
 def home_page(request):
     return render(request, "home.html", {"title": "Hello there ...", "my_list": [1, 2, 3, 4]})
@@ -10,8 +12,12 @@ def about_page(request):
     return render(request, "about.html", {"title": "About Us"})
 
 def contact_page(request):
-    print(request.path, request.method, request.user, request.POST)
-    return render(request, "contact.html", {"title": "Contact Us"})
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form = ContactForm()
+    context = {"title": "Contact Us", "form": form}
+    return render(request, "contact.html", context)
 
 def example_page(request):
     context = {"title": "example"}
