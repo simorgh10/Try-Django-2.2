2b7dab7e11ac725493a8e8e81ab950d3c17f87f4
simorgh10 <None>
2020-09-10 14:30:50 +0200

    Added about and contact views.

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def home_page(request):
     return HttpResponse("<h1>Hello World !</h1>")
+
+def about_page(request):
+    return HttpResponse("<h1>About us</h1>")
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact us</h1>")
