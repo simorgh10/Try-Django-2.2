9101ed3282edd94d7691a6cc3c992525545905ec
simorgh10 <None>
2020-09-10 19:06:16 +0200

    Fixed problem of multiple objects returned from the database.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,12 +1,16 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
 # Create your views here.
 from .models import BlogPost
 
 
 def blog_post_detail_page(request, slug):
-    obj = get_object_or_404(BlogPost, slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() >= 1:
+        obj = queryset.first()
+    else:
+        raise Http404
     template_name = "blog_post_detail_page.html"
     context = {"obj": obj}
     return render(request, template_name, context)
