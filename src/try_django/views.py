8e446efab219e9e4ed3026a031e10a6eafc39f5b
simorgh10 <None>
2020-09-10 15:24:10 +0200

    Example of template inheritance

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -6,7 +6,7 @@ def home_page(request):
     return render(request, "hello_world.html", {"title": "Hello there ..."})
 
 def about_page(request):
-    return render(request, "hello_world.html", {"title": "About Us"})
+    return render(request, "about.html", {"title": "About Us"})
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact Us"})
