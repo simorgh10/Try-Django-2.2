151bbb0412996cdd33f82a71fee201408dd89c85
simorgh10 <None>
2020-09-10 15:36:23 +0200

    Example of another way to render templates of any kind.

--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 
-from .views import home_page, about_page, contact_page
+from .views import home_page, about_page, contact_page, example_page
 
 urlpatterns = [
     path('', home_page),
     re_path(r'^pages?/', home_page),
     path('about/', about_page),
     path('contact/', contact_page),
+    path('example/', example_page),
     path('admin/', admin.site.urls),
 ]
--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
@@ -10,3 +11,10 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact Us"})
+
+def example_page(request):
+    context = {"title": "example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
+    return HttpResponse(rendered_item)
