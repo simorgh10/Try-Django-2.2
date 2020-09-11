216c940c917922e8c89a05434f8d1632327ccb71
simorgh10 <None>
2020-09-11 16:05:28 +0200

    Use a custom blog post manager to filter query set.

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,17 +2,25 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL
+
+class BlogPostManager(models.Manager):
+    def published(self):
+        now = timezone.now()
+        return self.get_queryset().filter(publish_date__lte=now)
 
 class BlogPost(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
-    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    objects = BlogPostManager()
 
     class Meta:
         ordering = ['-publish_date', '-updated', '-timestamp']
