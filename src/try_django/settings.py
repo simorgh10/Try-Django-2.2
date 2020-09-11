4799c8a758a3942c7407636658a40d4e707334ce
simorgh10 <None>
2020-09-11 17:55:13 +0200

    Configured settings to serve static files in DEBUG mode.

--- src/try_django/settings.py
+++ src/try_django/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOCAL_STATIC_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), "static_cdn_test")
+
+STATIC_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
+MEDIA_ROOT = os.path.join(LOCAL_STATIC_CDN_PATH, "media")
+MEDIA_URL = '/media/'
--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, re_path, include
 
@@ -33,3 +34,8 @@ urlpatterns = [
     path('example/', example_page),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
