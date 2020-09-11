7df1a8d258e0f07d29cd7d7dcef982ad3067b5ce
simorgh10 <None>
2020-09-11 19:40:53 +0200

    Lookup for blog posts corresponding to a search query and display them in search view.

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 
 # Create your models here.
+from django.db.models import Q
 from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL
@@ -11,12 +12,24 @@ class BlogPostQuerySet(models.QuerySet):
         now = timezone.now()
         return self.filter(publish_date__lte=now)
 
+    def search(self, query):
+        lookup = (Q(title__icontains=query) |
+                  Q(content__icontains=query) |
+                  Q(slug__icontains=query) |
+                  Q(user__first_name__icontains=query) |
+                  Q(user__last_name__icontains=query) |
+                  Q(user__username__icontains=query))
+        return self.filter(lookup)
+
 class BlogPostManager(models.Manager):
     def get_queryset(self):
         return BlogPostQuerySet(self.model, using=self._db)
 
     def published(self):
         return self.get_queryset().published()
+
+    def search(self, query):
+        return self.get_queryset().search(query)
 
 class BlogPost(models.Model):
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
--- src/searches/views.py
+++ src/searches/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import SearchQuery
+from blog.models import BlogPost
 
 
 def search_view(request):
@@ -9,7 +10,12 @@ def search_view(request):
     user = None
     if request.user.is_authenticated:
         user = request.user
+    context = {"query": query}
     if query:
         SearchQuery.objects.create(user=user, query=query)
-    context = {"query": query}
+        qs = BlogPost.objects.published().search(query)
+        if request.user.is_authenticated:
+            my_qs = BlogPost.objects.filter(user=request.user).search(query)
+            qs = (qs | my_qs).distinct()
+        context["blog_list"] = qs
     return render(request, "searches/view.html", context)
