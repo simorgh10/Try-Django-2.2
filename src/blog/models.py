d570c1d7d66f99f0f7f791c2cd0586ac853e5cb2
simorgh10 <None>
2020-09-11 13:45:08 +0200

    Added some navigation.

--- src/blog/models.py
+++ src/blog/models.py
@@ -10,3 +10,12 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+    def get_edit_url(self):
+        return f"{self.get_absolute_url()}/edit"
+
+    def get_delete_url(self):
+        return f"{self.get_absolute_url()}/delete"
