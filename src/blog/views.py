7c1a064b1da46608c11971807004469ad25cd409
simorgh10 <None>
2020-09-11 12:22:25 +0200

    Added the ability to delete a Blog Post.

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .forms import BlogPostModelForm
@@ -32,17 +32,23 @@ def blog_post_detail_view(request, slug):
     context = {"obj": obj}
     return render(request, template_name, context)
 
+@staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     form = BlogPostModelForm(request.POST or None, instance=obj)
     if form.is_valid():
         form.save()
     template_name = "form.html"
-    context = {"form": form, "title": f"Update {obj.title}"}
+    context = {"title": f"Update {obj.title}", "form": form}
     return render(request, template_name, context)
 
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
+    #if request.method == "POST":
+    if request.POST:
+        obj.delete()
+        return redirect("/blog")
     template_name = "blog/delete.html"
     context = {"obj": obj}
     return render(request, template_name, context)
