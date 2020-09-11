a6ab0d4c1d05fbe1f0df7b4c654091c867813432
simorgh10 <None>
2020-09-11 15:44:19 +0200

    Added publish date, timestamp and updated fields to blog post model.

--- src/blog/models.py
+++ src/blog/models.py
@@ -10,6 +10,12 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_date', '-updated', '-timestamp']
 
     def get_absolute_url(self):
         return f"/blog/{self.slug}"
